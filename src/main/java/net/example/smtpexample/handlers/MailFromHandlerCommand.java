cdbb3c11272bf46b7589c2d4bfcd5f2e34c6970d
VolLol <nurs.ypurs@gmail.com>
2020-05-06 22:30:25 +0300

    Add code to MailFromHandlerCommand for pass the test

--- src/main/java/net/example/smtpexample/handlers/MailFromHandlerCommand.java
+++ src/main/java/net/example/smtpexample/handlers/MailFromHandlerCommand.java
@@ -11,7 +11,7 @@ public class MailFromHandlerCommand extends BaseHandlerCommand {
 
     public MailFromHandlerCommand(SessionContext sessionContext) {
         super(sessionContext);
-        this.patternMailCompile = Pattern.compile("^(MAIL\\sFROM:)\\s(<)([a-zA-Z0-9]+@(contoso\\.com))(>)");
+        this.patternMailCompile = Pattern.compile("^MAIL\\sFROM:\\s<([a-zA-Z0-9]+@(\\w+)\\.\\w+)>");
     }
 
     @Override
@@ -19,13 +19,14 @@ public class MailFromHandlerCommand extends BaseHandlerCommand {
         ArrayList<String> answers = new ArrayList<>();
         Matcher matcher = patternMailCompile.matcher(inputLine);
         if (matcher.matches()) {
-            this.sessionContext.setMailFrom(matcher.group(3));
-            this.sessionContext.setSessionStateMailFromDone();
-            answers.add("250 2.1.0 Sender OK");
-        } else if (inputLine.contains("gmail")) {
-            answers.add("553 5.1.8 Sender e-mail address domain does not exist.");
-        }
-        else {
+            if (matcher.group(2).equals("contoso")) {
+                this.sessionContext.setMailFrom(matcher.group(1));
+                this.sessionContext.setSessionStateMailFromDone();
+                answers.add("250 2.1.0 Sender OK");
+            } else {
+                answers.add("553 5.1.8 Sender e-mail address domain does not exist.");
+            }
+        } else {
             answers.add("500 5.5.1 Syntax error, command unrecognized.");
         }
         return answers;
